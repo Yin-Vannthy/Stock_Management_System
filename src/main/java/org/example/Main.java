3539b6083fbad29e252ce78abd0c1ac30c6a7728
Yin-Vannthy <thygod942@gmail.com>
2024-03-05 12:05:09 +0700

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.sql.Connection;
+
+public class Main {
+    public static void main(String[] args) {
+        DbFunction db = new DbFunction();
+        Connection conn = db.connect_to_db("kshrd_db","postgres","admin");
+//        db.createTable(conn,"employees");
+//        db.insertRow(conn, "employees", "Vannthy", "Cambodia");
+        db.update(conn, "employees", 1, "Jisoo", "Korea");
+        db.readData(conn, "employees");
+
+    }
+}
